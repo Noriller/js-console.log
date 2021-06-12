75c503dd3945fec45f8fd55aa9ce6e85f47f98cb
Bruno Noriller <brunonoriller@gmail.com>
2021-06-12 12:47:57 -0300

    feat: :memo: adding readme

---
+++ README.md
@@ -0,0 +1,6 @@
+# In how many ways can you print in the console 50 times
+
+I was asked at an interview how to print in the console "Hello World" 50 times in as few lines as possible while not using loops.
+I, of course, answered: recursion.
+
+## Let's find out!
--- logs.test.js
+++ logs.test.js
@@ -1,5 +1,6 @@
 /*
-  I was asked at an interview how to print in the console "Hello World" 50 times in as few lines as possible while not using loops.I, of course, answered: recursion.
+  I was asked at an interview how to print in the console "Hello World" 50 times in as few lines as possible while not using loops.
+  I, of course, answered: recursion.
 
   Well... this lead me to this: in how many ways can you print in the console 50 times ?
 
